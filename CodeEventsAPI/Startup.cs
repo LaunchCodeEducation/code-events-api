10a9b6d4306cf0550f2e62ff1db7349ee7ece786
Vamp <vampiirecodes@gmail.com>
2020-03-25 22:13:01 -0500

    swagger: configure UI base

--- CodeEventsAPI/Startup.cs
+++ CodeEventsAPI/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.OpenApi.Models;
 
 namespace CodeEventsAPI {
   public class Startup {
@@ -19,6 +20,10 @@ namespace CodeEventsAPI {
     public IConfiguration Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services) {
+      // TODO: add to keyvault on deploy
+      // used to generate absolute resource links
+      ServerConfig.Origin = Configuration.GetValue<string>("ServerOrigin");
+
       // configure routing
       services.AddMvc();
       services.AddControllers();
@@ -32,6 +37,7 @@ namespace CodeEventsAPI {
           dbOptions.UseMySql(Configuration.GetConnectionString("Default"))
       );
 
+      // configure ADB2C auth handling
       services.AddAuthentication(AzureADB2CDefaults.AuthenticationScheme)
         .AddAzureADB2C(options => Configuration.Bind("AzureAdB2C", options));
       services.AddOptions();
@@ -39,8 +45,17 @@ namespace CodeEventsAPI {
         Configuration.GetSection("AzureAdB2C")
       );
 
-      // TODO: add to keyvault on deploy
-      ServerConfig.Origin = Configuration.GetValue<string>("ServerOrigin");
+      // configure swagger documentation
+      services.AddSwaggerGen(
+        options => options.SwaggerDoc(
+          "v1",
+          new OpenApiInfo {
+            Version = "v1",
+            Title = "Code Events API",
+            Description = "REST API for managing Code Events"
+          }
+        )
+      );
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
@@ -57,6 +72,15 @@ namespace CodeEventsAPI {
       app.UseEndpoints(
         endpoints => endpoints.MapControllers()
       ); // continue to Controller handlers
+
+      // configure swagger UI page
+      app.UseSwagger();
+      app.UseSwaggerUI(
+        options => options.SwaggerEndpoint(
+          "/swagger/v1/swagger.json",
+          "Code Events API Documentation"
+        )
+      );
 
 
       // automatic DB migrations
